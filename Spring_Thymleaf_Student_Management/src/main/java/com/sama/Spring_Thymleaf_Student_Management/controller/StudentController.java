d7baae76f4e34b199725454343a2a01a53ff9aa4
Sama <samamostafa507@gmail.com>
2025-11-12 10:54:55 +0200

    fixed updating student feature

--- Spring_Thymleaf_Student_Management/src/main/java/com/sama/Spring_Thymleaf_Student_Management/controller/StudentController.java
+++ Spring_Thymleaf_Student_Management/src/main/java/com/sama/Spring_Thymleaf_Student_Management/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.sama.Spring_Thymleaf_Student_Management.controller;
 
+import com.sama.Spring_Thymleaf_Student_Management.model.Course;
 import com.sama.Spring_Thymleaf_Student_Management.model.Student;
 import com.sama.Spring_Thymleaf_Student_Management.service.CourseService;
 import com.sama.Spring_Thymleaf_Student_Management.service.StudentService;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -44,6 +47,8 @@ public class StudentController {
     public String editStudentForm(@PathVariable("id") int id, Model model) {
         model.addAttribute("student", studentService.findStudent(id));
         model.addAttribute("pageTitle", "Edit Student");
+        List<Course> courses = courseService.viewCourses();
+        model.addAttribute("courses", courses);
         return "edit-student";
     }
 
